412513bf9987f06022b453fa95a4e857eb9502c1
haokee-git <19173155158@163.com>
2024-11-19 14:27:10 +0800

    2024.11.19

--- HaokeeEncoder/obj/x86/Release/net8.0-windows10.0.19041.0/win-x86/HaokeeEncoder.AssemblyInfo.cs
+++ HaokeeEncoder/obj/x86/Release/net8.0-windows10.0.19041.0/win-x86/HaokeeEncoder.AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("HaokeeEncoder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a33d98bfb8691a2c2f4ea5eb7818d7a1cc366d0d")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22b5a76a04087cbf59dcb37b07071e520c8bd837")]
 [assembly: System.Reflection.AssemblyProductAttribute("HaokeeEncoder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HaokeeEncoder")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.1")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows10.0.19041.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows10.0.17763.0")]
 
